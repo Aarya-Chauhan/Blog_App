d1248106d0395e6ea34d7c7fef2330a44c416629
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-07-08 20:57:43 +0530

    fixing bug

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -157,5 +157,28 @@ exports.deleteBlogController = async (req, res) => {
   };
 
 
-//GET || user-blogs
-exports.getUserBlogController = async(req,res) => {}
+//GET USER BLOG
+exports.userBlogControlller = async (req, res) => {
+  try {
+    const userBlog = await userModel.findById(req.params.id).populate("blogs");
+
+    if (!userBlog) {
+      return res.status(404).send({
+        success: false,
+        message: "blogs not found with this id",
+      });
+    }
+    return res.status(200).send({
+      success: true,
+      message: "user blogs",
+      userBlog,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      success: false,
+      message: "error in user blog",
+      error,
+    });
+  }
+};
