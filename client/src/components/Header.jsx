f1ba17f8ef22dc86d6eef7e97c76355a04b69f6b
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-07-13 22:33:10 +0530

    finishing up with header ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import {Box, AppBar, Toolbar, Button, Typography} from '@mui/material';
+import React, {useState} from 'react';
+import {Box, AppBar, Toolbar, Button, Typography, Tabs, Tab } from '@mui/material';
+import { Link } from 'react-router-dom';
 const Header = () => {
+  const [value,setValue] = useState()
   return (
     <>
     <AppBar position='sticky'>
@@ -8,10 +10,18 @@ const Header = () => {
             <Typography variant='h4'>
                 My Blog App
             </Typography>
+            <Box display={"flex"} marginLeft={"auto"} marginRight={"auto"}>
+              <Tabs textColor='inherit' value={value} onChange={(e,val) => {setValue(val)}} >
+                <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
+                <Tab label="My BLogs" LinkComponent={Link} to="/my-blogs" />
+
+              </Tabs>
+
+            </Box>
             <Box display={'flex'} marginLeft="auto">
-                <Button sx={{margin:1,color:'white'}}>Login</Button>
-                <Button sx={{margin:1,color:'white'}}>Register</Button>
-                <Button sx={{margin:1,color:'white'}}>Logout</Button>
+                <Button sx={{margin:1,color:'white'}} LinkComponent={Link} to="/login">Login</Button>
+                <Button sx={{margin:1,color:'white'}} LinkComponent={Link} to="/register">Register</Button>
+                <Button sx={{margin:1,color:'white'}} >Logout</Button>
             </Box>
         </Toolbar>
     </AppBar >
