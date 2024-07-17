75ea5807ed3ca768d85cb5077c28584a5cc124d6
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-07-17 22:50:13 +0530

    updating blog card

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -15,16 +15,6 @@ import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
 
 export default function BlogCard() {
   const [expanded, setExpanded] = React.useState(false);
