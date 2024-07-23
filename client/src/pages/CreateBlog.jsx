66a4cb3d3c12b0de59ce609a1d3980d4c6fac403
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-07-23 16:17:20 +0530

    creating post

--- client/src/pages/CreateBlog.jsx
+++ client/src/pages/CreateBlog.jsx
@@ -1,11 +1,114 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Box, InputLabel, TextField, Typography, Button } from '@mui/material'
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast"
+
 
 const CreateBlog = () => {
-  return (
-    <div>
-      helloooo!!!
-    </div>
-  )
-}
+    const [inputs,setInputs] = useState({
+        title:'',
+        description:'',
+        image:''
+    })
+    const id = localStorage.getItem("userId");
+    const navigate = useNavigate()
+    //handle change function
+    const handleChange = (e) => {
+        setInputs(prevState => ({
+            ...prevState,
+            [e.target.name] : e.target.value
+        }))
+    }
 
-export default CreateBlog
+    //form handle submit function
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const { data } = await axios.post("/api/v1/blog/create-blog", {
+            title: inputs.title,
+            description: inputs.description,
+            image: inputs.image,
+            user: id,
+          });
+          if (data?.success) {
+            toast.success("Blog Created");
+            navigate("/my-blogs");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      return (
+        <>
+          <form onSubmit={handleSubmit}>
+            <Box
+              width={"50%"}
+              border={3}
+              borderRadius={10}
+              padding={3}
+              margin="auto"
+              boxShadow={"10px 10px 20px #ccc"}
+              display="flex"
+              flexDirection={"column"}
+              marginTop="30px"
+            >
+              <Typography
+                variant="h2"
+                textAlign={"center"}
+                fontWeight="bold"
+                padding={3}
+                color="gray"
+              >
+                Create A Pots
+              </Typography>
+              <InputLabel
+                sx={{  mt: 2, fontSize: "24px", fontWeight: "bold" }}
+              >
+                Title
+              </InputLabel>
+              <TextField
+                name="title"
+                value={inputs.title}
+                onChange={handleChange}
+                margin="normal"
+                variant="outlined"
+                required
+              />
+              <InputLabel
+                sx={{ mt: 2, fontSize: "24px", fontWeight: "bold" }}
+              >
+                Description
+              </InputLabel>
+              <TextField
+                name="description"
+                value={inputs.description}
+                onChange={handleChange}
+                margin="normal"
+                variant="outlined"
+                required
+              />
+              <InputLabel
+                sx={{mb:1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
+              >
+                Image URL
+              </InputLabel>
+              <TextField
+                name="image"
+                value={inputs.image}
+                onChange={handleChange}
+                margin="normal"
+                variant="outlined"
+                required
+              />
+              <Button type="submit" color="primary" variant="contained">
+                SUBMIT
+              </Button>
+            </Box>
+          </form>
+        </>
+      );
+    };
+    
+    export default CreateBlog;
